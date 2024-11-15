0be8fadf6a0718c07511dabb64c99c4c2e92d96f
LEROY Noah <noah.leroy@etu.univ-amu.fr>
2024-11-15 16:07:59 +0100

    Maladie simple

---
+++ test/TP3/MaladieTest.java
@@ -0,0 +1,45 @@
+package TP3;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MaladieTest {
+    @Test
+    public void testCreationMaladie() {
+        // Test de la création de la maladie
+        Maladie maladie = new Maladie("Maladie Débilitante Chronique", "MDC", 1,5);
+        assertEquals("Maladie Débilitante Chronique", maladie.getNomComplet());
+        assertEquals("MDC", maladie.getNomAbrege());
+        assertEquals(0, maladie.getNiveauActuel());
+        assertEquals(100, maladie.getNiveauMaximum());
+    }
+    @Test
+    public void testAugmenterNiveau() {
+        Maladie maladie = new Maladie("Maladie Débilitante Chronique", "MDC",1, 5);
+        maladie.augmenterNiveau(3);
+        assertEquals(4, maladie.getNiveauActuel());
+
+        maladie.augmenterNiveau(5);
+        assertEquals(5, maladie.getNiveauActuel());
+    }
+    @Test
+    public void testEstLetale() {
+        Maladie maladie = new Maladie("Maladie Débilitante Chronique", "MDC", 1, 5);
+        assertFalse(maladie.estLetale());
+
+        maladie.augmenterNiveau(5);
+        assertTrue(maladie.estLetale());
+    }
+
+    @Test
+    public void testDiminueNiveau() {
+        Maladie maladie = new Maladie("Maladie Débilitante Chronique", "MDC", 1, 5);
+        maladie.augmenterNiveau(2);
+        maladie.diminuerNiveau(1);
+        assertEquals(2, maladie.getNiveauActuel());
+
+        maladie.diminuerNiveau(1);
+        assertEquals(1, maladie.getNiveauActuel());
+    }
+}
